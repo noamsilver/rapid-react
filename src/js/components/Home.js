6f7e466089714f622f3365bc688cf7a83934a1ee
Noam Silver <noamsilver@gmail.com>
2018-03-01 23:38:57 +0200

    Fixed the text selection bug and displying current username.

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -6,11 +6,13 @@ import '../../styles/home.css';
 class Home extends Component {
   constructor(props) {
     super(props);
+    const token = utils.getSessionToken();
     this.state = {
       clickPosition: undefined,
       dx: 0,
       dy: 0,
-      position: utils.getSessionToken().position
+      position: token.position,
+      username: token.username
     }
 
     this.mouseDown = this.mouseDown.bind(this);
@@ -23,7 +25,6 @@ class Home extends Component {
   }
 
   mouseDown(event) {
-    event.preventDefault();
     if (event.pageX && event.pageY) {
       this.setState({
         clickPosition: { x: event.pageX, y: event.pageY }
@@ -33,6 +34,7 @@ class Home extends Component {
 
   mouseMove(event) {
     event.preventDefault();
+    event.stopPropagation();
     if (this.state.clickPosition) {
       this.setState({
         dx: event.pageX - this.state.clickPosition.x,
@@ -42,7 +44,6 @@ class Home extends Component {
   }
 
   mouseUp(event) {
-    event.preventDefault();
     event.stopPropagation();
     if (this.state.clickPosition) {
       const x = this.state.position.x + this.state.dx;
@@ -61,6 +62,7 @@ class Home extends Component {
     return (
       <div className="content" onMouseUp={this.mouseUp} onMouseLeave={this.mouseUp} onMouseMove={this.mouseMove}>
         <div className="welcome">Welcome to Drag&amp;Drop Forever
+          <span className="username">{'User: ' + this.state.username}</span>
           <Link to="/login" className="logout" onClick={this.clickLogout}>Logout</Link>
         </div>
         <div className="drag image" style={{ left: this.state.position.x + this.state.dx, top: this.state.position.y + this.state.dy }} onMouseDown={this.mouseDown} onMouseMove={this.mouseMove} onMouseUp={this.mouseUp}/>
